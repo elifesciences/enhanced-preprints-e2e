ab50f456c2d151951444e3d6a38a6c61a6e20f84
aisha <a.kapan@elifesciences.org>
2023-09-12 17:03:03 +0100

    Install dev dependencies for wiremock & write first test in jest

---
+++ tests/title.test.ts
@@ -0,0 +1,13 @@
+import { IWireMockRequest, IWireMockResponse, WireMock } from "wiremock-captain"
+
+describe("that it displays title on the page", () => {
+    const wireMock = new WireMock("http://localhost:8080");
+    beforeAll(async () => {
+        const request: IWireMockRequest  = {method: "GET", endpoint: "/docmaps"};
+        const response: IWireMockResponse = {status: 200, body: []};
+        wireMock.register(request, response);
+    });
+    it("display the title", () => {
+
+    }); 
+});
