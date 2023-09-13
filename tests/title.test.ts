06d7d3d33335484587381a51dac8a1e927005174
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-13 12:48:41 +0100

    feat: update title.test.ts to ensure proper client termination
    The 'title.test.ts' test file has been updated to instantiate the Temporal Client outside of the 'beforeAll' function. Additionally, an 'afterAll' function has been added to terminate the workflow after the 'title' test has completed.

--- tests/title.test.ts
+++ tests/title.test.ts
@@ -1,13 +1,18 @@
 import { Client } from "@temporalio/client";
 
 describe('that it displays title on the page', () => {
+  const temporal = new Client();
+
   beforeAll(async () => {
-    const temporal = new Client();
     await temporal.workflow.start('pollDocMapIndex', {
       taskQueue: 'epp',
       workflowId: 'title',
       args: ['http://wiremock:8080/docmaps', '1 minute'],
     });
+  });
+
+  afterAll(async () => {
+    await temporal.workflow.getHandle('title').terminate('end of title test');
   });
 
   it('display the title', () => {
