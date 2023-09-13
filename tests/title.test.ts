c16d26ae1a68ce7b8ed4fae6acb161ab4fc9055f
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-13 15:23:12 +0100

    feat: add axios library and use it in tests
    This commit adds the axios library to the project dependencies and uses it in 'title.test.ts' to change and reset the state of wiremock. The yarn.lock file has also been updated to reflect these new dependencies.

--- tests/title.test.ts
+++ tests/title.test.ts
@@ -1,9 +1,15 @@
 import { Client } from '@temporalio/client';
+import axios from 'axios';
 
 describe('that it displays title on the page', () => {
   const temporal = new Client();
 
   beforeAll(async () => {
+    // Change state of wiremock to 'title'
+    await axios.put('http://localhost:8080/__admin/scenarios/docmap/state', {
+      state: 'title'
+    });
+
     await temporal.workflow.start('pollDocMapIndex', {
       taskQueue: 'epp',
       workflowId: 'title',
@@ -12,6 +18,8 @@ describe('that it displays title on the page', () => {
   });
 
   afterAll(async () => {
+    // Reset state
+    await axios.put('http://localhost:8080/__admin/scenarios/docmap/state');
     await temporal.workflow.getHandle('title').terminate('end of title test');
   });
 
