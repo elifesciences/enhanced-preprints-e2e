9c9784b3cd72b2e850b951ca6c880095c705be02
Dave Price <d.price@elifesciences.org>
2023-09-13 12:39:37 +0100

    start workflow as part of a test

--- tests/title.test.ts
+++ tests/title.test.ts
@@ -1,4 +1,15 @@
+import { Client } from "@temporalio/client";
+
 describe('that it displays title on the page', () => {
+  beforeAll(async () => {
+    const temporal = new Client();
+    await temporal.workflow.start('pollDocMapIndex', {
+      taskQueue: 'epp',
+      workflowId: 'title',
+      args: ['http://wiremock:8080/docmaps', '1 minute'],
+    });
+  });
+
   it('display the title', () => {
   });
 });
