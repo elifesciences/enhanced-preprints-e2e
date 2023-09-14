07f1e88e3ab70528130f1b1f1c49579ddd31bd55
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-14 21:33:05 +0100

    Add ability to reset database and buckets after running a test

--- tests/title.spec.ts
+++ tests/title.spec.ts
@@ -1,9 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { Client } from '@temporalio/client';
 import axios from 'axios';
+import Dockerode from 'dockerode';
+import DockerodeCompose from '@rkesters/dockerode-compose';
 
 test.describe('that it displays title on the page', () => {
   const temporal = new Client();
+  const dockerode = new Dockerode();
+  const serviceResetbucket = new DockerodeCompose(dockerode, './docker-compose.reset.yaml', 'resetbucket');
+  const serviceResetdb = new DockerodeCompose(dockerode, './docker-compose.reset.yaml', 'resetdb');
 
   test.beforeAll(async () => {
     // Change state of wiremock to 'title'
@@ -21,8 +26,11 @@ test.describe('that it displays title on the page', () => {
   test.afterAll(async () => {
     // Reset state
     await axios.put('http://localhost:8080/__admin/scenarios/docmap/state');
+
+    await serviceResetbucket.up();
+    await serviceResetdb.up();
+
     await temporal.workflow.getHandle('title').terminate('end of title test');
-    await axios.delete('http://localhost:3000/preprints/000001v1');
   });
 
   test('display the title', async ({ page }) => {
