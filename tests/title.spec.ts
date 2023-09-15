b20c6c25c8ab3da72c9095c4aaf336b968331672
aisha <a.kapan@elifesciences.org>
2023-09-15 15:54:37 +0100

    Initial attempt at clearing the test object data from minio epp bucket

--- tests/title.spec.ts
+++ tests/title.spec.ts
@@ -2,10 +2,18 @@ import { test, expect } from '@playwright/test';
 import { Client } from '@temporalio/client';
 import axios from 'axios';
 import { generateWorkflowId } from '../utils/generate-workflow-id';
+import { S3Client } from '@aws-sdk/client-s3';
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 test.describe('that it displays title on the page', () => {
   const temporal = new Client();
   const workflowId = generateWorkflowId('title');
+  const minioClient = new S3Client({
+    credentials: {accessKeyId: 'minio', secretAccessKey: 'miniotest'},
+    endpoint: "http://localhost:9100",
+    forcePathStyle: true,
+    region: "us-east-1",
+  });
 
   test.beforeAll(async () => {
     await temporal.workflow.start('pollDocMapIndex', {
@@ -14,10 +22,12 @@ test.describe('that it displays title on the page', () => {
       args: ['http://wiremock:8080/docmaps/title', '1 minute'],
     });
   });
-
+  
+  
   test.afterAll(async () => {
     await temporal.workflow.getHandle(workflowId).terminate('end of title test');
     await axios.delete('http://localhost:3000/preprints/title-msidv1');
+    await minioClient.send(new DeleteObjectCommand({Bucket: "epp", Key: "automation/title-msid"}));
   });
 
   test('display the title', async ({ page }) => {
