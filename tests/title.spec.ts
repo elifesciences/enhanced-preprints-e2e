317a9e1f0cc9576d04dc83ba8657e51b2e26d22c
viktoria.mahmud <viktoria.nedyalkova@endava.com>
2023-09-15 13:16:00 +0300

    Change workflowId

--- tests/title.spec.ts
+++ tests/title.spec.ts
@@ -13,7 +13,7 @@ test.describe('that it displays title on the page', () => {
 
     await temporal.workflow.start('pollDocMapIndex', {
       taskQueue: 'epp',
-      workflowId: 'title',
+      workflowId: `title-${new Date().getTime()}`,
       args: ['http://wiremock:8080/docmaps', '1 minute'],
     });
   });
