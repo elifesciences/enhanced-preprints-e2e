07b53fc41fff5ba300c948d54d2afa13da9a544a
Will Byrne <will.alex.byrne@gmail.com>
2023-09-15 12:26:00 +0100

    re-organise the mocks into folders for each test

--- tests/title.spec.ts
+++ tests/title.spec.ts
@@ -7,21 +7,14 @@ test.describe('that it displays title on the page', () => {
   const temporal = new Client();
   const workflowId = generateWorkflowId('title');
   test.beforeAll(async () => {
-    // Change state of wiremock to 'title'
-    await axios.put('http://localhost:8080/__admin/scenarios/docmap/state', {
-      state: 'title',
-    });
-
     await temporal.workflow.start('pollDocMapIndex', {
       taskQueue: 'epp',
       workflowId,
-      args: ['http://wiremock:8080/docmaps', '1 minute'],
+      args: ['http://wiremock:8080/docmaps/title', '1 minute'],
     });
   });
 
   test.afterAll(async () => {
-    // Reset state
-    await axios.put('http://localhost:8080/__admin/scenarios/docmap/state');
     await temporal.workflow.getHandle(workflowId).terminate('end of title test');
     await axios.delete('http://localhost:3000/preprints/000001v1');
   });
