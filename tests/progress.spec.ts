21158615232b32e961ede0037347570a64e2460d
Will Byrne <will.alex.byrne@gmail.com>
2023-09-19 12:33:11 +0100

    refactor test name from preview to progress

--- tests/progress.spec.ts
+++ tests/progress.spec.ts
@@ -6,9 +6,9 @@ import { deleteS3EppFolder } from '../utils/delete-s3-epp-folder';
 import { createTemporalClient } from '../utils/create-temporal-client';
 import { config } from '../utils/config';
 
-test.describe('manuscript available in preview', () => {
+test.describe('progress a manuscript through the manifestations', () => {
   let temporal: any;
-  const workflowId = generateWorkflowId('preview');
+  const workflowId = generateWorkflowId('progress');
   const minioClient = createS3Client();
 
   test.beforeAll(async () => {
@@ -16,37 +16,37 @@ test.describe('manuscript available in preview', () => {
     await temporal.workflow.start('pollDocMapIndex', {
       taskQueue: 'epp',
       workflowId,
-      args: ['http://wiremock:8080/docmaps/preview', '30 seconds'],
+      args: ['http://wiremock:8080/docmaps/progress', '30 seconds'],
     });
   });
 
   test.afterAll(async () => {
     await Promise.all([
       temporal.workflow.getHandle(workflowId).terminate('end of preview test'),
-      axios.delete(`${config.api_url}/preprints/preview-msidv1`),
-      deleteS3EppFolder(minioClient, 'preview-msid'),
-      axios.put(`${config.wiremock_url}/__admin/scenarios/preview/state`),
+      axios.delete(`${config.api_url}/preprints/progress-msidv1`),
+      deleteS3EppFolder(minioClient, 'progress-msid'),
+      axios.put(`${config.wiremock_url}/__admin/scenarios/progress/state`),
     ]);
   });
 
-  test('and later published', async ({ page }) => {
-    const response1 = await page.goto(`${config.client_url}/previews/preview-msid`);
+  test('successful progression of manuscript', async ({ page }) => {
+    const response1 = await page.goto(`${config.client_url}/previews/progress-msid`);
     expect(response1?.status()).toBeGreaterThan(400);
 
-    await axios.put(`${config.wiremock_url}/__admin/scenarios/preview/state`, { state: 'Preview' });
+    await axios.put(`${config.wiremock_url}/__admin/scenarios/progress/state`, { state: 'Preview' });
 
     // Wait for preview to become available.
     await expect(async () => {
-      const response2 = await page.goto(`${config.client_url}/previews/preview-msid`);
+      const response2 = await page.goto(`${config.client_url}/previews/progress-msid`);
       expect(response2?.status()).toBe(200);
     }).toPass();
-    const response3 = await page.goto(`${config.client_url}/reviewed-preprints/preview-msid`);
+    const response3 = await page.goto(`${config.client_url}/reviewed-preprints/progress-msid`);
     expect(response3?.status()).toBe(404);
 
-    await axios.put(`${config.wiremock_url}/__admin/scenarios/preview/state`, { state: 'Published' });
+    await axios.put(`${config.wiremock_url}/__admin/scenarios/progress/state`, { state: 'Published' });
 
     await expect(async () => {
-      const response4 = await page.goto(`${config.client_url}/reviewed-preprints/preview-msid`);
+      const response4 = await page.goto(`${config.client_url}/reviewed-preprints/progress-msid`);
       expect(response4?.status()).toBe(200);
     }).toPass();
   });
