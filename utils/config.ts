782e479d62add9cd3cd462ae9adb7be80756eaaa
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-19 11:26:57 +0100

    feat: update temporal client setup and configuration
    - Imported and used Temporal Client in title.spec.ts
    - Changed 'temporal_url' to 'temporal_address' in utils/config.ts
    - Added 'wiremock_url' to utils/config.ts
    - Simplified Temporal client creation in utils/create-temporal-client.ts

--- utils/config.ts
+++ utils/config.ts
@@ -2,6 +2,6 @@ export const config = {
   minio_url: process.env.minio_url ?? 'http://localhost:9100',
   client_url: process.env.client_url ?? 'http://localhost:3001',
   api_url: process.env.api_url ?? 'http://localhost:3000',
-  temporal_url: process.env.temporal_url ?? '',
+  temporal_address: process.env.temporal_address ?? undefined,
+  wiremock_url: process.env.wiremock_url ?? 'http://localhost:8080',
 };
-
