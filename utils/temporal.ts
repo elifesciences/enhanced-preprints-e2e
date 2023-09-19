527d4c42c4195dcac53547bc9f0d87b3970187ec
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-19 15:25:50 +0100

    feat: refactor code and fix formatting issues
    Refactored import statements in 'progress.spec.ts' and 'title.spec.ts' for better readability. Fixed formatting issues in 'temporal.ts' such as missing semicolons and inappropriate usage of template literals.

--- utils/temporal.ts
+++ utils/temporal.ts
@@ -2,7 +2,7 @@ import { config } from './config';
 import { Client, Connection } from '@temporalio/client';
 
 export const generateWorkflowId = (prefix: string): string => {
-  if (prefix.trim().length === 0) { 
+  if (prefix.trim().length === 0) {
     throw Error('Empty prefix');
   }
 
@@ -12,18 +12,18 @@ export const generateWorkflowId = (prefix: string): string => {
 export const createTemporalClient = async () => {
   const connection = await Connection.connect({ address: config.temporal_address });
   return new Client({ connection });
-}
+};
 
 export const startWorkflow = async (testName: string, workflowId: string, client: Client) => {
   return client.workflow.start('pollDocMapIndex', {
     taskQueue: 'epp',
     workflowId,
+    // Because this url is submitted to temporal it must be the url available to the temporal worker.
     args: [`http://wiremock:8080/docmaps/${testName}`, '1 minute'],
   });
-}
+};
 
-export const stopWorkflow = async (workflowId: string, client: Client, message = `end of test`) => {
+export const stopWorkflow = async (workflowId: string, client: Client, message = 'end of test') => {
   return client.workflow.getHandle(workflowId)
     .terminate(message);
 };
-
