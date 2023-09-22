b564ed22a2046a8015e03f7a323e9a5199157a2c
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-22 16:50:54 +0100

    feat: Improve readability and add timeline checks in revised test
    This commit updates the revised preprint test to improve readability and
    add further checks on the review timeline. It removes excessive blank
    lines and corrects spelling mistakes in the comments. It also adds
    additional check for the latest revision in the review timeline.

--- tests/revised.spec.ts
+++ tests/revised.spec.ts
@@ -5,8 +5,8 @@ import { createS3Client } from '../utils/create-s3-client';
 import { deleteS3EppFolder } from '../utils/delete-s3-epp-folder';
 import { config } from '../utils/config';
 import {
-    createTemporalClient, generateWorkflowId, startWorkflow, stopWorkflow,
-  } from '../utils/temporal';
+  createTemporalClient, generateWorkflowId, startWorkflow, stopWorkflow,
+} from '../utils/temporal';
 
 test.describe('revised preprint', () => {
   let temporal: Client;
@@ -39,20 +39,20 @@ test.describe('revised preprint', () => {
 
     await expect(page.locator('.article-status__text')).toContainText('Published from the original preprint after peer review and assessment by eLife.');
     await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.1');
-    
-    // 3rd child is 2nd description details (<dd>) from the timeline defintion list 
-    const reviewTimelinePageLocatorV1 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(3)')
+
+    // 3rd child is 2nd description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV1 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(3)');
     await expect(reviewTimelinePageLocatorV1).toContainText('Reviewed preprint version 1');
-    await expect(reviewTimelinePageLocatorV1.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)')
+    await expect(reviewTimelinePageLocatorV1.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
 
     await page.getByLabel('Reviewed preprint version 2').click();
     await expect(page.locator('.article-status__text')).toContainText('Revised by authors after peer review.');
     await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.2');
 
-    // 1st child is 1st description details (<dd>) from the timeline defintion list 
-    const reviewTimelinePageLocatorV2 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)')
+    // 1st child is 1st description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV2 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
     await expect(reviewTimelinePageLocatorV2).toContainText('Reviewed preprint version 2');
-    await expect(reviewTimelinePageLocatorV2.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)')
+    await expect(reviewTimelinePageLocatorV2.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
 
     await page.getByLabel('Reviewed preprint version 1').click();
     await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.1');
@@ -62,5 +62,9 @@ test.describe('revised preprint', () => {
     await expect(page.locator('.article-status__text')).toContainText('Revised by authors after peer review.');
     await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.2');
 
+    // 1st child is 1st description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorLatest = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
+    await expect(reviewTimelinePageLocatorLatest).toContainText('Reviewed preprint version 2');
+    await expect(reviewTimelinePageLocatorLatest.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
   });
 });
