510e56a703dd0ca6ba15bc653ec54c8531ec9d77
aisha <a.kapan@elifesciences.org>
2023-09-22 13:49:36 +0100

    Create initial structure for tests

---
+++ tests/revisions.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import axios from 'axios';
+import { Client } from '@temporalio/client';
+import { createS3Client } from '../utils/create-s3-client';
+import { deleteS3EppFolder } from '../utils/delete-s3-epp-folder';
+import { config } from '../utils/config';
+import {
+    createTemporalClient, generateWorkflowId, startWorkflow, stopWorkflow,
+  } from '../utils/temporal';
+
+
