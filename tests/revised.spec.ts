80aa880cdc81c008d8f19f4ef9a980fecf5e059c
Scott Aubrey <scott@aubrey.org.uk>
2023-09-22 15:57:00 +0100

    Add assertions for the doi version on each version page, and navigating between versions. Also check that the msid URL loads the second version

--- tests/revised.spec.ts
+++ tests/revised.spec.ts
@@ -30,13 +30,26 @@ test.describe('revised preprint', () => {
 
   test('revised preprints are available', async ({ page }) => {
     await expect(async () => {
-      const response = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv1`);
-      expect(response?.status()).toBe(200);
+      // For the test to succeed, we need to wait for both versions to be imported
+      const responsev2 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv2`);
+      expect(responsev2?.status()).toBe(200);
+      const responsev1 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv1`);
+      expect(responsev1?.status()).toBe(200);
     }).toPass();
-    await expect(page.locator('.article-status__text')).toContainText('Published from the original preprint after peer review and assessment by eLife.');
 
-    const response = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv2`);
-    expect(response?.status()).toBe(200);
+    await expect(page.locator('.article-status__text')).toContainText('Published from the original preprint after peer review and assessment by eLife.');
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.1');
+
+    await page.getByLabel('Reviewed preprint version 2').click();
     await expect(page.locator('.article-status__text')).toContainText('Revised by authors after peer review.');
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.2');
+
+    await page.getByLabel('Reviewed preprint version 1').click();
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.1');
+
+    const responseMsid = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msid`);
+    expect(responseMsid?.status()).toBe(200);
+    await expect(page.locator('.article-status__text')).toContainText('Revised by authors after peer review.');
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-msid.2');
   });
 });
