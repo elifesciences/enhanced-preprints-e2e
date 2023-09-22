99aa87e9056e9b29221d7934a79758c64de722ed
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-22 14:49:53 +0100

    feat: update reviews.spec.ts and revise.spec.ts
    Update reviews.spec.ts to dynamically delete preprints instead of hardcoding the name.
    Add a new test to revised.spec.ts to verify the availability of revised preprints.

--- tests/revised.spec.ts
+++ tests/revised.spec.ts
@@ -8,4 +8,32 @@ import {
     createTemporalClient, generateWorkflowId, startWorkflow, stopWorkflow,
   } from '../utils/temporal';
 
+test.describe('revised preprint', () => {
+  let temporal: Client;
+  const name = 'revised';
+  const workflowId = generateWorkflowId(name);
+  const minioClient = createS3Client();
 
+  test.beforeAll(async () => {
+    temporal = await createTemporalClient();
+    await startWorkflow(name, workflowId, temporal);
+  });
+
+  test.afterAll(async () => {
+    await Promise.all([
+      stopWorkflow(workflowId, temporal),
+      axios.delete(`${config.api_url}/preprints/${name}-msidv1`),
+      axios.delete(`${config.api_url}/preprints/${name}-msidv2`),
+      deleteS3EppFolder(minioClient, `${name}-msid`),
+    ]);
+  });
+
+  test('revised preprints are available', async ({ page }) => {
+    await expect(async () => {
+      const response = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv1`);
+      expect(response?.status()).toBe(200);
+    }).toPass();
+    const response = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv2`);
+    expect(response?.status()).toBe(200);
+  });
+});
