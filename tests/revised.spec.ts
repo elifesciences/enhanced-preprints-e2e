816b2cc491df9bc071f5de866a5afdcade0ad28e
Nathan Lisgo <nathan@lisgo.co.uk>
2023-09-25 20:19:52 +0100

    feat: refactor revised preprint tests
    Refactored the 'revised preprint' tests to move the setup and teardown
    processes into beforeEach and afterEach blocks. This ensures the workflow
    and associated resources are properly handled for each test case. Also,
    enhanced assertions to verify the versions, article details, and review
    timeline for multiple versions of revised preprints.

--- tests/revised.spec.ts
+++ tests/revised.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import axios from 'axios';
+import { Client } from '@temporalio/client';
 import { createS3Client } from '../utils/create-s3-client';
 import { deleteS3EppFolder } from '../utils/delete-s3-epp-folder';
 import { config } from '../utils/config';
@@ -9,14 +10,32 @@ import {
 
 test.describe('revised preprint', () => {
   const minioClient = createS3Client();
+  let temporal: Client;
+  let name: string;
+  let workflowId: string;
 
-  test('higher versions of revised preprint are available', async ({ page }) => {
-    // Setup.
-    const temporal = await createTemporalClient();
-    const name = 'revised-multiple';
-    const workflowId = generateWorkflowId(name);
+  test.beforeEach(async (_, testInfo) => {
+    temporal = await createTemporalClient();
+    name = testInfo.title.includes('multiple') ? 'revised-multiple' : 'revised';
+    workflowId = generateWorkflowId(name);
+
     await startWorkflow(name, workflowId, temporal);
+  });
 
+  test.afterEach(async () => {
+    const tearDowns: Promise<any>[] = [
+      stopWorkflow(workflowId, temporal),
+      deleteS3EppFolder(minioClient, `${name}-msid`),
+    ];
+
+    const versions = (name === 'revised-multiple') ? 3 : 2;
+    for (let i = 1; i <= versions; i += 1) {
+      tearDowns.push(axios.delete(`${config.api_url}/preprints/${name}-msidv${i}`));
+    }
+    await Promise.all(tearDowns);
+  });
+
+  test('multiple versions of revised preprint are available', async ({ page }) => {
     // For the test to succeed, we need to wait for all versions to be imported
     await expect(async () => {
       const responsev3 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv3`);
@@ -31,23 +50,37 @@ test.describe('revised preprint', () => {
       expect(responsev1?.status()).toBe(200);
     }).toPass();
 
-    // Tear down.
-    await Promise.all([
-      stopWorkflow(workflowId, temporal),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv1`),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv2`),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv3`),
-      deleteS3EppFolder(minioClient, `${name}-msid`),
-    ]);
+    await expect(page.locator('.article-status__text')).toContainText('Published from the original preprint after peer review and assessment by eLife.');
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-multiple-msid.1');
+    await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation');
+
+    // 5th child is 3rd description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV1 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(5)');
+    await expect(reviewTimelinePageLocatorV1).toContainText('Reviewed preprint version 1');
+    await expect(reviewTimelinePageLocatorV1.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
+
+    await page.getByLabel('Reviewed preprint version 2').click();
+    await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
+    await expect(page.locator('.article-status__text')).toContainText('Revised by authors after peer review.');
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-multiple-msid.2');
+
+    // 3rd child is 2nd description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV2 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(3)');
+    await expect(reviewTimelinePageLocatorV2).toContainText('Reviewed preprint version 2');
+    await expect(reviewTimelinePageLocatorV2.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
+
+    await page.getByLabel('Reviewed preprint version 3').click();
+    await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
+    await expect(page.locator('.article-status__text')).toContainText('Revised by authors after peer review.');
+    await expect(page.locator('.descriptors__identifier')).toContainText('https://doi.org/10.7554/eLife.revised-multiple-msid.3');
+
+    // 1st child is 1st description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV3 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
+    await expect(reviewTimelinePageLocatorV3).toContainText('Reviewed preprint version 3');
+    await expect(reviewTimelinePageLocatorV3.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
   });
 
   test('revised preprints are available', async ({ page }) => {
-    // Setup.
-    const temporal = await createTemporalClient();
-    const name = 'revised';
-    const workflowId = generateWorkflowId(name);
-    await startWorkflow(name, workflowId, temporal);
-
     // For the test to succeed, we need to wait for both versions to be imported
     await expect(async () => {
       const responsev2 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv2`);
@@ -90,13 +123,5 @@ test.describe('revised preprint', () => {
     const reviewTimelinePageLocatorLatest = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
     await expect(reviewTimelinePageLocatorLatest).toContainText('Reviewed preprint version 2');
     await expect(reviewTimelinePageLocatorLatest.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
-
-    // Tear down.
-    await Promise.all([
-      stopWorkflow(workflowId, temporal),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv1`),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv2`),
-      deleteS3EppFolder(minioClient, `${name}-msid`),
-    ]);
   });
 });
