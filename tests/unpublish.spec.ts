5a4716861b3f0461c23b5d6b5433da3024bd4963
aisha <a.kapan@elifesciences.org>
2023-09-29 16:40:32 +0100

    Add setup test to check that published article is initially there

---
+++ tests/unpublish.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+import axios from 'axios';
+import { Client } from '@temporalio/client';
+import { createS3Client } from '../utils/create-s3-client';
+import { deleteS3EppFolder } from '../utils/delete-s3-epp-folder';
+import { config } from '../utils/config';
+import {
+  createTemporalClient, generateWorkflowId, startWorkflow, stopWorkflow,
+} from '../utils/temporal';
+
+test.describe('unpublished preprint', () => {
+  let temporal: Client;
+  const name = 'unpublished';
+  const workflowId = generateWorkflowId(name);
+  const minioClient = createS3Client();
+
+  test.beforeAll(async () => {
+    temporal = await createTemporalClient();
+    await startWorkflow(name, workflowId, temporal);
+  });
+
+  test.afterAll(async () => {
+    await Promise.all([
+      stopWorkflow(workflowId, temporal),
+      axios.delete(`${config.api_url}/preprints/${name}-msidv1`),
+      deleteS3EppFolder(minioClient, `${name}-msid`),
+    ]);
+  });
+
+  test('preprints can be unpublished', async ({ page }) => {
+    await expect(async () => {
+      const responsev1 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv1`);
+      expect(responsev1?.status()).toBe(200);
+    }).toPass();
+
+    await expect(page.locator('h1.title')).toBeVisible();
+    await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation');
+
+  });
+});
