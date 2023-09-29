796e316d8b60c7d576609ce5222bb1352754bb56
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-09-29 11:21:53 +0100

    Add checks to verify successful publication of multiple versions of a manuscript (#38)
    * Create wiremock docmaps for higher version revised preprints
    
    * feat: update dates and DOIs in dummy-2.json
    
    - Update published dates, DOIs, URLs in review-article, reply, and
      evaluation-summary output types.
    - Update the 'happened' date in manuscript-published status.
    
    * feat: update identifier in dummy-2.json and refactor test cases
    
    Update the identifier in 'mock-data/docmaps/revised-multiple/dummy-2.json'
    from 'revised-msid' to 'revised-multiple-msid'. Refactor tests in 'revised.spec.ts'
    to separate setup, test, and teardown steps into individual blocks for readability
    and maintenance. Also, include checks for higher versions of revised preprints availability.
    
    * feat: refactor revised preprint tests
    
    Refactored the 'revised preprint' tests to move the setup and teardown
    processes into beforeEach and afterEach blocks. This ensures the workflow
    and associated resources are properly handled for each test case. Also,
    enhanced assertions to verify the versions, article details, and review
    timeline for multiple versions of revised preprints.
    
    * feat: expand mock data in dummy-2.json
    
    Add additional steps and data to the mock data file for improved testing. Include actions, assertions, inputs and output details associated with each step. The steps extend from _:b9 to _:b11.
    
    * feat: add support for multiple revisions in revised preprints
    
    Updated the mock data and test cases to support multiple revisions in the revised preprints. The test cases now check for the correct version number and DOI in the page's metadata and review timeline. The mock data now includes an additional version for each preprint.
    
    * chore(deps): update ghcr.io/elifesciences/enhanced-preprints-biorxiv-xslt-api docker tag to master-f70c388d-20230925.1340
    
    * fix(deps): update typescript-eslint monorepo to v6.7.3
    
    * chore(deps): update ghcr.io/elifesciences/enhanced-preprints-client docker tag to master-c2941a3f-20230926.0357
    
    * add flake so users running nix can run the playwright tests
    
    * chore(deps): update ghcr.io/elifesciences/enhanced-preprints-import-worker docker tag to master-1da69ae8-20230926.0828
    
    * fix(deps): update dependency @aws-sdk/client-s3 to v3.420.0
    
    * fix(deps): update dependency axios to v1.5.1
    
    * make the nixpkgs point to a specific known working version in the flake
    
    * chore(deps): update ghcr.io/elifesciences/enhanced-preprints-client docker tag to master-3d29431a-20230927.0923
    
    * chore(deps): update ghcr.io/elifesciences/enhanced-preprints-server docker tag to master-a31c9f74-20230927.0845
    
    * move test and setup to separate file for version test
    
    * refactor test to remove revised-multiple steps no longer needed
    
    * feat: update playwright test command in package.json
    
    Remove worker parameter from the playwright test command to enhance
    test stability and avoid potential concurrency issues.
    
    * style: remove trailing whitespace in index.json
    
    * feat: limit playwright test to single worker
    
    Update the test script in package.json to limit playwright to use only one worker. This change should help manage resource utilization during testing.
    
    ---------
    
    Co-authored-by: aisha <a.kapan@elifesciences.org>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Will Byrne <will.alex.byrne@gmail.com>

--- tests/versions.spec.ts
+++ tests/versions.spec.ts
@@ -8,31 +8,42 @@ import {
   createTemporalClient, generateWorkflowId, startWorkflow, stopWorkflow,
 } from '../utils/temporal';
 
-test.describe('revised preprint', () => {
+test.describe('versions', () => {
   const minioClient = createS3Client();
   let temporal: Client;
-  const name = 'revised';
+  const name = 'versions';
   let workflowId: string;
 
   // eslint-disable-next-line no-empty-pattern
-  test.beforeAll(async () => {
+  test.beforeEach(async () => {
     temporal = await createTemporalClient();
     workflowId = generateWorkflowId(name);
 
     await startWorkflow(name, workflowId, temporal);
   });
 
-  test.afterAll(async () => {
-    await Promise.all([
+  test.afterEach(async () => {
+    const tearDowns: Promise<any>[] = [
       stopWorkflow(workflowId, temporal),
       deleteS3EppFolder(minioClient, `${name}-msid`),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv1`),
-      axios.delete(`${config.api_url}/preprints/${name}-msidv2`),
-    ]);
+    ];
+
+    for (let i = 1; i <= 4; i += 1) {
+      tearDowns.push(axios.delete(`${config.api_url}/preprints/${name}-msidv${i}`));
+    }
+    await Promise.all(tearDowns);
   });
 
-  test('revised preprints are available', async ({ page }) => {
-    // For the test to succeed, we need to wait for both versions to be imported
+  test('multiple versions of a preprint are available', async ({ page }) => {
+    // For the test to succeed, we need to wait for all versions to be imported
+    await expect(async () => {
+      const responsev4 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv4`);
+      expect(responsev4?.status()).toBe(200);
+    }).toPass();
+    await expect(async () => {
+      const responsev3 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv3`);
+      expect(responsev3?.status()).toBe(200);
+    }).toPass();
     await expect(async () => {
       const responsev2 = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msidv2`);
       expect(responsev2?.status()).toBe(200);
@@ -46,8 +57,8 @@ test.describe('revised preprint', () => {
     await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.1`);
     await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation');
 
-    // 3rd child is 2nd description details (<dd>) from the timeline definition list
-    const reviewTimelinePageLocatorV1 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(3)');
+    // 7th child is 4th description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV1 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(7)');
     await expect(reviewTimelinePageLocatorV1).toHaveText('Reviewed preprint version 1');
     await expect(reviewTimelinePageLocatorV1.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
 
@@ -56,23 +67,40 @@ test.describe('revised preprint', () => {
     await expect(page.locator('.article-status__text')).toHaveText('Revised by authors after peer review.');
     await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.2`);
 
-    // 1st child is 1st description details (<dd>) from the timeline definition list
-    const reviewTimelinePageLocatorV2 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
+    // 5th child is 3rd description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV2 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(5)');
     await expect(reviewTimelinePageLocatorV2).toHaveText('Reviewed preprint version 2');
     await expect(reviewTimelinePageLocatorV2.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
 
-    await page.getByLabel('Reviewed preprint version 1').click();
-    await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.1`);
+    await page.getByLabel('Reviewed preprint version 3').click();
+    await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
+    await expect(page.locator('.article-status__text')).toHaveText('Revised by authors after peer review.');
+    await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.3`);
+
+    // 3rd child is 2nd description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV3 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(3)');
+    await expect(reviewTimelinePageLocatorV3).toHaveText('Reviewed preprint version 3');
+    await expect(reviewTimelinePageLocatorV3.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
+
+    await page.getByLabel('Reviewed preprint version 4').click();
+    await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
+    await expect(page.locator('.article-status__text')).toHaveText('Revised by authors after peer review.');
+    await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.4`);
+
+    // 1st child is 1st description details (<dd>) from the timeline definition list
+    const reviewTimelinePageLocatorV4 = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
+    await expect(reviewTimelinePageLocatorV4).toHaveText('Reviewed preprint version 4');
+    await expect(reviewTimelinePageLocatorV4.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
 
     const responseMsid = await page.goto(`${config.client_url}/reviewed-preprints/${name}-msid`);
     expect(responseMsid?.status()).toBe(200);
     await expect(page.locator('h1.title')).toHaveText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
     await expect(page.locator('.article-status__text')).toHaveText('Revised by authors after peer review.');
-    await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.2`);
+    await expect(page.locator('.descriptors__identifier')).toHaveText(`https://doi.org/10.7554/eLife.${name}-msid.4`);
 
     // 1st child is 1st description details (<dd>) from the timeline definition list
     const reviewTimelinePageLocatorLatest = page.locator('.review-timeline__list>.review-timeline__event:nth-child(1)');
-    await expect(reviewTimelinePageLocatorLatest).toHaveText('Reviewed preprint version 2');
+    await expect(reviewTimelinePageLocatorLatest).toHaveText('Reviewed preprint version 4');
     await expect(reviewTimelinePageLocatorLatest.locator('+.review-timeline__date .review-timeline__description')).toContainText('(this version)');
   });
 });
