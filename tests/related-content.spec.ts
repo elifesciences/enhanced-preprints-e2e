b4578279f8b8a7747f9f96befa0868d216f3ef6c
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2024-03-15 17:04:38 +0000

    Add a starter related content test file

---
+++ tests/related-content.spec.ts
@@ -0,0 +1,37 @@
+import { test } from '@playwright/test';
+import axios from 'axios';
+import { Client } from '@temporalio/client';
+import { createS3Client } from '../utils/create-s3-client';
+import { deleteS3EppFolder } from '../utils/delete-s3-epp-folder';
+import { config } from '../utils/config';
+import {
+  createTemporalClient, generateScheduleId, startScheduledImportWorkflow, stopScheduledImportWorkflow,
+} from '../utils/temporal';
+import { EppPage } from './page-objects/epp-page';
+
+test.describe('reviewed preprint with related content', () => {
+  let temporal: Client;
+  const name = 'related-content';
+  const scheduleId = generateScheduleId(name);
+  const minioClient = createS3Client();
+
+  test.beforeAll(async () => {
+    temporal = await createTemporalClient();
+    await startScheduledImportWorkflow(name, scheduleId, temporal);
+  });
+
+  test.afterAll(async () => {
+    await Promise.all([
+      stopScheduledImportWorkflow(scheduleId, temporal),
+      axios.delete(`${config.api_url}/preprints/${name}-msidv1`),
+      deleteS3EppFolder(minioClient, `${name}-msid`),
+      deleteS3EppFolder(minioClient, `state/${name}`),
+    ]);
+  });
+
+  test('test related content is coming through', async ({ page }) => {
+    const eppPage = new EppPage(page, name);
+    await eppPage.gotoReviewsPage();
+    await eppPage.reloadAndAssertStatus(200);
+  });
+});
