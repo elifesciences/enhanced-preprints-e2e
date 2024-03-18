f8f47fa9fbcea05399104cb7f32dccda1362d622
Nathan Lisgo <nathan@lisgo.co.uk>
2024-03-18 14:47:10 +0000

    feat: update related content assertion in eppPage tests
    The index for the related content assertion in the eppPage tests has been
    modified. The index has been changed from 0 to 1 for the 'Hearing: Letting
    the calcium flow' content.

--- tests/related-content.spec.ts
+++ tests/related-content.spec.ts
@@ -33,6 +33,6 @@ test.describe('reviewed preprint with related content', () => {
     const eppPage = new EppPage(page, name);
     await eppPage.gotoReviewsPage();
     await eppPage.reloadAndAssertStatus(200);
-    await eppPage.assertRelatedContent(0, 'Hearing: Letting the calcium flow');
+    await eppPage.assertRelatedContent(1, 'Hearing: Letting the calcium flow');
   });
 });
