cef3b634614d060d9e8baeb9086531be2f260ffb
Nathan Lisgo <nathan@lisgo.co.uk>
2024-07-04 22:50:24 +0100

    Refactor tests to reduce repetition and improve clarity [elifesciences/enhanced-preprints-issues#1135]

--- tests/progress.spec.ts
+++ tests/progress.spec.ts
@@ -20,7 +20,7 @@ const setUp = async (name: string, scheduleId: string, s3Client: S3Client) => {
   return temporal;
 };
 
-const tearDown = async (name: string, s3Client: S3Client, scheduleId: string, temporal: Client, versions: number = 1) => {
+const tearDown = async (name: string, s3Client: S3Client, scheduleId: string, temporal: Client, versions: number) => {
   const deleteRequests = [];
 
   for (let i = 1; i <= versions; i += 1) {
@@ -42,196 +42,130 @@ const switchState = async (name: string) => {
 
 test.describe('progress a manuscript through the manifestations', () => {
   const minioClient = createS3Client();
+  const temporalClients: Record<string, Client> = {};
+  const scheduleIds: Record<string, string> = {};
+  const versions: Record<string, number> = {};
 
-  test.describe('empty-to-preview', () => {
-    let temporal: Client;
-    const name = 'progress--empty-to-preview';
-    const scheduleId = generateScheduleId(name);
-
-    test.beforeEach(async () => {
-      temporal = await setUp(name, scheduleId, minioClient);
-    });
-
-    test.afterEach(async () => {
-      await tearDown(name, minioClient, scheduleId, temporal);
-    });
-
-    test('check', async ({ page }) => {
-      const eppPage = new EppPage(page, name);
-
-      await eppPage.gotoPreviewPage({ status: 404 });
-      await eppPage.gotoArticlePage({ status: 404 });
-
-      await switchState(name);
-
-      // Wait for preview to become available.
-      await eppPage.gotoPreviewPage();
-      await eppPage.reloadAndAssertStatus(200);
-      await eppPage.gotoArticlePage({ status: 404 });
-    });
+  // eslint-disable-next-line no-empty-pattern
+  test.beforeEach(async ({}, testInfo) => {
+    scheduleIds[testInfo.title] = generateScheduleId(testInfo.title);
+    temporalClients[testInfo.title] = await setUp(testInfo.title, scheduleIds[testInfo.title], minioClient);
   });
 
-  test.describe('preview-to-reviews', () => {
-    let temporal: Client;
-    const name = 'progress--preview-to-reviews';
-    const scheduleId = generateScheduleId(name);
-
-    test.beforeEach(async () => {
-      temporal = await setUp(name, scheduleId, minioClient);
-    });
-
-    test.afterEach(async () => {
-      await tearDown(name, minioClient, scheduleId, temporal);
-    });
-
-    test('check', async ({ page }) => {
-      const eppPage = new EppPage(page, name);
-
-      // Wait for preview to become available.
-      await eppPage.gotoPreviewPage();
-      await eppPage.reloadAndAssertStatus(200);
-      await eppPage.gotoArticlePage({ status: 404 });
-
-      await switchState(name);
-
-      // Wait for reviewed preprint to become available.
-      await eppPage.gotoArticlePage();
-      await eppPage.reloadAndAssertStatus(200);
-    });
+  // eslint-disable-next-line no-empty-pattern
+  test.afterEach(async ({}, testInfo) => {
+    await tearDown(testInfo.title, minioClient, scheduleIds[testInfo.title], temporalClients[testInfo.title], versions[testInfo.title] ?? 1);
   });
 
-  test.describe('reviews-to-preview-revised', () => {
-    let temporal: Client;
-    const name = 'progress--reviews-to-preview-revised';
-    const scheduleId = generateScheduleId(name);
+  test('progress--empty-to-preview', async ({ page }, testInfo) => {
+    const eppPage = new EppPage(page, testInfo.title);
 
-    test.beforeEach(async () => {
-      temporal = await setUp(name, scheduleId, minioClient);
-    });
+    await eppPage.gotoPreviewPage({ status: 404 });
+    await eppPage.gotoArticlePage({ status: 404 });
 
-    test.afterEach(async () => {
-      await tearDown(name, minioClient, scheduleId, temporal, 2);
-    });
+    await switchState(testInfo.title);
 
-    test('check', async ({ page }) => {
-      const eppPage = new EppPage(page, name);
-
-      // Wait for reviewed preprint to become available.
-      await eppPage.gotoArticlePage();
-      await eppPage.reloadAndAssertStatus(200);
-
-      await switchState(name);
-
-      // Wait for preview of revised preprint to become available.
-      await eppPage.gotoPreviewPage({ version: 2 });
-      await eppPage.reloadAndAssertStatus(200);
-      await eppPage.gotoArticlePage({ version: 2, status: 404 });
-      // Ensure that umbrella id still works with preview available
-      await eppPage.gotoArticlePage();
-      await eppPage.reloadAndAssertStatus(200);
-    });
+    // Wait for preview to become available.
+    await eppPage.gotoPreviewPage();
+    await eppPage.reloadAndAssertStatus(200);
+    await eppPage.gotoArticlePage({ status: 404 });
   });
 
-  test.describe('preview-revised-to-revised', () => {
-    let temporal: Client;
-    const name = 'progress--preview-revised-to-revised';
-    const scheduleId = generateScheduleId(name);
+  test('progress--preview-to-reviews', async ({ page }, testInfo) => {
+    const eppPage = new EppPage(page, testInfo.title);
 
-    test.beforeEach(async () => {
-      temporal = await setUp(name, scheduleId, minioClient);
-    });
+    // Wait for preview to become available.
+    await eppPage.gotoPreviewPage();
+    await eppPage.reloadAndAssertStatus(200);
+    await eppPage.gotoArticlePage({ status: 404 });
 
-    test.afterEach(async () => {
-      await tearDown(name, minioClient, scheduleId, temporal, 2);
-    });
+    await switchState(testInfo.title);
 
-    test('check', async ({ page }) => {
-      const eppPage = new EppPage(page, name);
-
-      // Wait for preview of revised preprint to become available.
-      await eppPage.gotoPreviewPage({ version: 2 });
-      await eppPage.reloadAndAssertStatus(200);
-      await eppPage.gotoArticlePage({ version: 2, status: 404 });
-      // Ensure that umbrella id still works with preview available
-      await eppPage.gotoArticlePage();
-      await eppPage.reloadAndAssertStatus(200);
-
-      await switchState(name);
-
-      // Wait for revised preprint to become available.
-      await eppPage.gotoArticlePage({ version: 2 });
-      await eppPage.reloadAndAssertStatus(200);
-    });
+    // Wait for reviewed preprint to become available.
+    await eppPage.gotoArticlePage();
+    await eppPage.reloadAndAssertStatus(200);
   });
 
-  test.describe('revised-to-version-of-record', () => {
-    let temporal: Client;
-    const name = 'progress--revised-to-version-of-record';
-    const scheduleId = generateScheduleId(name);
+  test('progress--reviews-to-preview-revised', async ({ page }, testInfo) => {
+    versions[testInfo.title] = 2;
+    const eppPage = new EppPage(page, testInfo.title);
 
-    test.beforeEach(async () => {
-      temporal = await setUp(name, scheduleId, minioClient);
-    });
+    // Wait for reviewed preprint to become available.
+    await eppPage.gotoArticlePage();
+    await eppPage.reloadAndAssertStatus(200);
 
-    test.afterEach(async () => {
-      await tearDown(name, minioClient, scheduleId, temporal, 3);
-    });
+    await switchState(testInfo.title);
 
-    test('check', async ({ page }) => {
-      const eppPage = new EppPage(page, name);
-
-      // Wait for revised preprint to become available.
-      await eppPage.gotoArticlePage({ version: 2 });
-      await eppPage.reloadAndAssertStatus(200);
-
-      await switchState(name);
-
-      // Wait for Version of Record summary to become available.
-      await expect(async () => {
-        const response = await eppPage.reload();
-        expect(response?.status()).toBe(200);
-        await eppPage.assertTimelineEventText(1, 'Version of Record');
-        await eppPage.assertTimelineDetailText(1, 'June 7, 2023');
-      }).toPass();
-    });
+    // Wait for preview of revised preprint to become available.
+    await eppPage.gotoPreviewPage({ version: 2 });
+    await eppPage.reloadAndAssertStatus(200);
+    await eppPage.gotoArticlePage({ version: 2, status: 404 });
+    // Ensure that umbrella id still works with preview available
+    await eppPage.gotoArticlePage();
+    await eppPage.reloadAndAssertStatus(200);
   });
 
-  test.describe('version-of-record-to-version-of-record-correction', () => {
-    let temporal: Client;
-    const name = 'progress--version-of-record-to-version-of-record-correction';
-    const scheduleId = generateScheduleId(name);
+  test('progress--preview-revised-to-revised', async ({ page }, testInfo) => {
+    versions[testInfo.title] = 2;
+    const eppPage = new EppPage(page, testInfo.title);
 
-    test.beforeEach(async () => {
-      temporal = await setUp(name, scheduleId, minioClient);
-    });
+    // Wait for preview of revised preprint to become available.
+    await eppPage.gotoPreviewPage({ version: 2 });
+    await eppPage.reloadAndAssertStatus(200);
+    await eppPage.gotoArticlePage({ version: 2, status: 404 });
+    // Ensure that umbrella id still works with preview available
+    await eppPage.gotoArticlePage();
+    await eppPage.reloadAndAssertStatus(200);
 
-    test.afterEach(async () => {
-      await tearDown(name, minioClient, scheduleId, temporal, 3);
-    });
+    await switchState(testInfo.title);
 
-    test('check', async ({ page }) => {
-      const eppPage = new EppPage(page, name);
+    // Wait for revised preprint to become available.
+    await eppPage.gotoArticlePage({ version: 2 });
+    await eppPage.reloadAndAssertStatus(200);
+  });
 
-      // Wait for Version of Record summary to become available.
-      await eppPage.gotoArticlePage({ version: 2 });
-      await expect(async () => {
-        const response = await eppPage.reload();
-        expect(response?.status()).toBe(200);
-        await eppPage.assertTimelineEventText(1, 'Version of Record');
-        await eppPage.assertTimelineDetailText(1, 'June 7, 2023');
-      }).toPass();
+  test('progress--revised-to-version-of-record', async ({ page }, testInfo) => {
+    versions[testInfo.title] = 3;
+    const eppPage = new EppPage(page, testInfo.title);
 
-      await switchState(name);
+    // Wait for revised preprint to become available.
+    await eppPage.gotoArticlePage({ version: 2 });
+    await eppPage.reloadAndAssertStatus(200);
 
-      // Wait for Version of Record summary with correction to become available.
-      await expect(async () => {
-        const response = await eppPage.reload();
-        expect(response?.status()).toBe(200);
-        await eppPage.assertTimelineEventText(1, 'Version of Record');
-        await eppPage.assertTimelineDetailText(1, 'July 6, 2023');
-      }).toPass();
-      await eppPage.assertTimelineEventText(2, 'Version of Record');
-      await eppPage.assertTimelineDetailText(2, 'June 7, 2023');
-    });
+    await switchState(testInfo.title);
+
+    // Wait for Version of Record summary to become available.
+    await expect(async () => {
+      const response = await eppPage.reload();
+      expect(response?.status()).toBe(200);
+      await eppPage.assertTimelineEventText(1, 'Version of Record');
+      await eppPage.assertTimelineDetailText(1, 'June 7, 2023');
+    }).toPass();
+  });
+
+  test('progress--version-of-record-to-version-of-record-correction', async ({ page }, testInfo) => {
+    versions[testInfo.title] = 3;
+    const eppPage = new EppPage(page, testInfo.title);
+
+    // Wait for Version of Record summary to become available.
+    await eppPage.gotoArticlePage({ version: 2 });
+    await expect(async () => {
+      const response = await eppPage.reload();
+      expect(response?.status()).toBe(200);
+      await eppPage.assertTimelineEventText(1, 'Version of Record');
+      await eppPage.assertTimelineDetailText(1, 'June 7, 2023');
+    }).toPass();
+
+    await switchState(testInfo.title);
+
+    // Wait for Version of Record summary with correction to become available.
+    await expect(async () => {
+      const response = await eppPage.reload();
+      expect(response?.status()).toBe(200);
+      await eppPage.assertTimelineEventText(1, 'Version of Record');
+      await eppPage.assertTimelineDetailText(1, 'July 6, 2023');
+    }).toPass();
+    await eppPage.assertTimelineEventText(2, 'Version of Record');
+    await eppPage.assertTimelineDetailText(2, 'June 7, 2023');
   });
 });
