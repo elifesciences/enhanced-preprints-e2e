f900587cdd7826e66552291cc47ccae3c41e36d4
Nathan Lisgo <nathan@lisgo.co.uk>
2024-07-05 23:13:26 +0100

    Refactor threshold tests for improved readability and efficiency [elifesciences/enhanced-preprints-issues#1135]

--- tests/threshold.spec.ts
+++ tests/threshold.spec.ts
@@ -17,108 +17,102 @@ test.describe('threshold', () => {
     workflowIds,
   } = setupClientAndScheduleStores();
 
-  test.describe('hit threshold and reject', () => {
-    const name = 'threshold--reject';
-    test.beforeEach(async () => {
-      const {
-        scheduleId,
-        scheduleHandle,
-        workflowId,
-      } = await setupTemporal({
-        name,
-        s3Client: minioClient,
-        duration: '1 minute',
-        docMapThreshold: 1,
-      });
-      scheduleIds[name] = scheduleId;
-      scheduleHandles[name] = scheduleHandle;
-      workflowIds[name] = workflowId;
+  // eslint-disable-next-line no-empty-pattern
+  test.beforeEach(async ({}, testInfo) => {
+    const {
+      scheduleId,
+      scheduleHandle,
+      workflowId,
+    } = await setupTemporal({
+      name: testInfo.title,
+      s3Client: minioClient,
+      duration: '1 minute',
+      docMapThreshold: 1,
     });
-
-    test.afterEach(async () => {
-      await Promise.all([
-        stopScheduledImportWorkflow(scheduleIds[name], await createTemporalClient()),
-        deleteS3EppFolder(minioClient, `state/${name}`),
-      ]);
-    });
-
-    test('docmap threshold triggers workflow pause', async () => {
-      const workflowHandle = getWorkflowHandle(workflowIds[name], await createTemporalClient());
-
-      // Wait for threshold pause to commence and check output.
-      await expect(async () => {
-        const response = await workflowHandle.query<{ awaitingApproval: number, docMapUrls: string[] }>('awaitingApproval');
-        expect(response.docMapUrls).not.toBeNull();
-        expect(response.docMapUrls).toHaveLength(2);
-      }).toPass();
-
-      // Send approval signal with value of false.
-      await workflowHandle.signal('approval', false);
-      await expect(async () => {
-        const workflowStatus = await workflowHandle.describe().then((wf) => wf.status.name);
-        expect(workflowStatus).toBe('COMPLETED');
-      }).toPass();
-
-      // Confirm no longer awaiting approval.
-      const response = await workflowHandle.query('awaitingApproval');
-      expect(response).toBeNull();
-    });
+    scheduleIds[testInfo.title] = scheduleId;
+    scheduleHandles[testInfo.title] = scheduleHandle;
+    workflowIds[testInfo.title] = workflowId;
   });
 
-  test.describe('hit threshold and approve', () => {
-    const name = 'threshold--approve';
-    test.beforeEach(async () => {
-      const {
-        scheduleId,
-        scheduleHandle,
-        workflowId,
-      } = await setupTemporal({
-        name,
-        s3Client: minioClient,
-        duration: '1 minute',
-        docMapThreshold: 1,
-      });
-      scheduleIds[name] = scheduleId;
-      scheduleHandles[name] = scheduleHandle;
-      workflowIds[name] = workflowId;
-    });
-
-    test.afterEach(async () => {
+  // eslint-disable-next-line no-empty-pattern
+  test.afterEach(async ({}, testInfo) => {
+    if (testInfo.title === 'threshold--reject') {
+      await Promise.all([
+        stopScheduledImportWorkflow(scheduleIds[testInfo.title], await createTemporalClient()),
+        deleteS3EppFolder(minioClient, `state/${testInfo.title}`),
+      ]);
+    } else if (testInfo.title === 'threshold--approve') {
       await trashTemporal({
-        name,
+        name: testInfo.title,
         s3Client: minioClient,
-        scheduleId: scheduleIds[name],
+        scheduleId: scheduleIds[testInfo.title],
       });
       await Promise.all([
-        deleteS3EppFolder(minioClient, `${name}-msid-2`),
-        axios.delete(`${config.api_url}/preprints/${name}-msid-2v1`),
+        deleteS3EppFolder(minioClient, `${testInfo.title}-msid-2`),
+        axios.delete(`${config.api_url}/preprints/${testInfo.title}-msid-2v1`),
       ]);
-    });
+    }
+  });
 
-    test('import continues when approval signal true', async () => {
-      const workflowHandle = getWorkflowHandle(workflowIds[name], await createTemporalClient());
+  [
+    {
+      // test docmap threshold triggers workflow pause.
+      name: 'reject',
+      expectation: async (name: string) => {
+        const workflowHandle = getWorkflowHandle(workflowIds[name], await createTemporalClient());
 
-      // Wait for threshold pause to commence and check output.
-      await expect(async () => {
-        const response = await workflowHandle.query<{ awaitingApproval: number, docMapUrls: string[] }>('awaitingApproval');
-        expect(response.docMapUrls).not.toBeNull();
-        expect(response.docMapUrls).toHaveLength(2);
-      }).toPass();
+        // Wait for threshold pause to commence and check output.
+        await expect(async () => {
+          const response = await workflowHandle.query<{ awaitingApproval: number, docMapUrls: string[] }>('awaitingApproval');
+          expect(response.docMapUrls).not.toBeNull();
+          expect(response.docMapUrls).toHaveLength(2);
+        }).toPass();
 
-      // Send approval signal with value of true.
-      await workflowHandle.signal('approval', true);
-      await expect(async () => {
-        const workflowStatus = await workflowHandle.describe().then((wf) => wf.status.name);
-        expect(workflowStatus).toBe('COMPLETED');
-      }).toPass();
+        // Send approval signal with value of false.
+        await workflowHandle.signal('approval', false);
+        await expect(async () => {
+          const workflowStatus = await workflowHandle.describe().then((wf) => wf.status.name);
+          expect(workflowStatus).toBe('COMPLETED');
+        }).toPass();
 
-      // Confirm no longer awaiting approval.
-      const response = await getWorkflowHandle(workflowIds[name], await createTemporalClient()).query('awaitingApproval');
-      expect(response).toBeNull();
+        // Confirm no longer awaiting approval.
+        const response = await workflowHandle.query('awaitingApproval');
+        expect(response).toBeNull();
+      },
+    },
+    {
+      // test import continues when approval signal true.
+      name: 'approve',
+      expectation: async (name: string) => {
+        const workflowHandle = getWorkflowHandle(workflowIds[name], await createTemporalClient());
 
-      // Check previews available on server.
-      expect((await axios.get(`${config.api_url}/api/preprints/${name}-msidv1?previews`).then((r) => r.status))).toBe(200);
-      expect((await axios.get(`${config.api_url}/api/preprints/${name}-msid-2v1?previews`).then((r) => r.status))).toBe(200);
+        // Wait for threshold pause to commence and check output.
+        await expect(async () => {
+          const response = await workflowHandle.query<{ awaitingApproval: number, docMapUrls: string[] }>('awaitingApproval');
+          expect(response.docMapUrls).not.toBeNull();
+          expect(response.docMapUrls).toHaveLength(2);
+        }).toPass();
+
+        // Send approval signal with value of true.
+        await workflowHandle.signal('approval', true);
+        await expect(async () => {
+          const workflowStatus = await workflowHandle.describe().then((wf) => wf.status.name);
+          expect(workflowStatus).toBe('COMPLETED');
+        }).toPass();
+
+        // Confirm no longer awaiting approval.
+        const response = await getWorkflowHandle(workflowIds[name], await createTemporalClient()).query('awaitingApproval');
+        expect(response).toBeNull();
+
+        // Check previews available on server.
+        expect((await axios.get(`${config.api_url}/api/preprints/${name}-msidv1?previews`).then((r) => r.status))).toBe(200);
+        expect((await axios.get(`${config.api_url}/api/preprints/${name}-msid-2v1?previews`).then((r) => r.status))).toBe(200);
+      },
+    },
+  ].forEach(({ name, expectation }) => {
+    // eslint-disable-next-line no-empty-pattern
+    test(`threshold--${name}`, async ({}, testInfo) => {
+      await expectation(testInfo.title);
     });
   });
 });
