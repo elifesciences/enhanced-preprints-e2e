b63c4285ceb65c5055d32348890967353fa83ef5
Nathan Lisgo <nathan@lisgo.co.uk>
2024-11-17 10:24:10 +0000

    Add form submission and test for manuscript data import process

---
+++ tests/import-manuscript-data.spec.ts
@@ -0,0 +1,95 @@
+import { test } from '@playwright/test';
+import { ImportManuscriptDataPage } from './page-objects/import-manuscript-data-page';
+
+const prepareManuscriptData = (id: string, optional?: true) => {
+  const requiredPreprint = {
+    id,
+    doi: '10.1101/654321',
+  };
+  
+  const optionalPreprint = {
+    ...requiredPreprint,
+    publishedDate: '2023-01-02',
+    url: 'www.google.com',
+    content: ['this is some content'],
+    license: 'Creative Commons',
+    corrections: [{
+      content: ['Corrections Content'],
+      correctedDate: '2023-02-03',
+    }],
+  };
+  
+  const requiredVersion = {
+    versionIdentifier: 'v42',
+    id: `${id}.1`,
+    doi: '10.1101/123456',
+    preprint: requiredPreprint,
+  };
+  
+  const optionalEvaluation = {
+    date: '2023-01-02',
+    doi: '12.3456/123456',
+    reviewType: 'review-article',
+    contentUrls: ['www.google.com'],
+    participants: [{
+      name: 'Joe Bloggs',
+      role: 'Dungeon Master',
+      institution: {
+        name: 'Dungeons Inc.',
+        location: 'Fey Wilds',
+      },
+    }],
+  };
+  
+  const optionalVersion = {
+    ...requiredVersion,
+    preprint: optionalPreprint,
+    publishedDate: '2023-03-04',
+    sentForReviewDate: '2023-03-05',
+    peerReview: {
+      evaluationSummary: optionalEvaluation,
+      reviews: [optionalEvaluation],
+      authorResponse: optionalEvaluation,
+    },
+    reviewedDate: '2023-03-06',
+    authorResponseDate: '2023-03-07',
+    license: 'Creative Commons',
+    corrections: [{
+      content: ['Corrections Content'],
+      correctedDate: '2023-02-03',
+    }],
+    content: ['This is some more content'],
+  };
+  
+  const optionalManuscript = {
+    doi: `10.${id}`,
+    volume: '11',
+    eLocationId: 'cyberspace',
+    publishedDate: '2023-01-02',
+    subjects: ['Alchemy', 'Mad Science'],
+    relatedContent: [{
+      type: 'book',
+      title: 'Monster Manual',
+      url: 'www.google.com',
+      description: 'its a book',
+      thumbnail: 'www.google.com',
+    }],
+  };
+  
+  return JSON.stringify(optional ? {
+    id,
+    versions: [optionalVersion],
+    manuscript: optionalManuscript,
+  } : {
+    id,
+    versions: [requiredVersion],
+  });
+};
+
+test.describe('Import Manuscript Data', () => {
+  test('publish content via import manuscript data form', async ({ page }) => {
+    const importManuscriptDataPage = new ImportManuscriptDataPage(page);
+    await importManuscriptDataPage.gotoForm();
+    await importManuscriptDataPage.fillAndSubmitForm(prepareManuscriptData('1234'));
+  });
+});
--- tests/page-objects/import-manuscript-data-page.ts
+++ tests/page-objects/import-manuscript-data-page.ts
@@ -3,24 +3,48 @@ import {
 } from '@playwright/test';
 import { config } from '../../utils/config';
 
-class ImportManuscriptDataPage {
+export class ImportManuscriptDataPage {
   private page: Page;
   
   readonly input: Locator;
   
-  readonly nameSpaceSelector: Locator;
+  readonly namespaceSelector: Locator;
   
   readonly submit: Locator;
+  
+  readonly workflowLink: Locator;
 
   constructor(thePage: Page) {
     this.page = thePage;
     this.input = this.page.locator('#manuscript-data');
-    this.nameSpaceSelector = this.page.locator('#temporal_namespace');
+    this.namespaceSelector = this.page.locator('#temporal_namespace');
     this.submit = this.page.locator('button[type="submit"]');
+    this.workflowLink = this.page.locator('body a');
   }
   
   async gotoForm(): Promise<void> {
-    const response = await this.page.goto(`${config.import_controller}/input`);
+    const response = await this.page.goto(`${config.import_controller_url}/input`);
     expect(response?.status()).toBe(200);
+  }
+
+  async fillAndSubmitForm(text: string): Promise<void> {
+    await this.input.fill(text);
+    await this.namespaceSelector.selectOption('default');
+
+    const [submitResponse] = await Promise.all([
+      this.page.waitForNavigation({ waitUntil: 'networkidle' }),
+      this.submit.click(),
+    ]);
+
+    expect(submitResponse?.status()).toBe(200);
+
+    await expect(this.page.locator('body')).toContainText('Import started');
+
+    const [importLinkResponse] = await Promise.all([
+      this.page.waitForNavigation({ waitUntil: 'load' }),
+      this.workflowLink.click(),
+    ]);
+
+    expect(importLinkResponse?.status()).toBe(200);
   }
 }
