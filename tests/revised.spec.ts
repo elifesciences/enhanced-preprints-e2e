814806e15ed22dbaa3e02626e4325fb345d058fb
Will Byrne <will.alex.byrne@gmail.com>
2024-11-29 15:10:25 +0000

    replace calls to navigateToVersion and expandTimeline where not neccesary for tests

--- tests/revised.spec.ts
+++ tests/revised.spec.ts
@@ -33,33 +33,20 @@ test.describe('revised preprint', () => {
     await eppPage.assertDoi('https://doi.org/10.7554/000001.1');
     await eppPage.assertTitleText('OpenApePose: a database of annotated ape photographs for pose estimation');
 
-    await eppPage.expandTimeline();
-
-    await eppPage.assertTimelineDetailText(2, 'v1');
-    await eppPage.assertTimelineEventThisVersion(2);
-
-    await eppPage.navigateToVersion(2, true);
+    await eppPage.gotoArticlePage({ version: 2 });
+    await eppPage.reloadAndAssertStatus(200);
     await eppPage.assertTitleText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
     await eppPage.assertArticleStatusText('Revised by authors');
     await eppPage.assertDoi('https://doi.org/10.7554/000001.2');
 
-    await eppPage.expandTimeline();
-
-    await eppPage.assertTimelineDetailText(1, 'v2');
-    await eppPage.assertTimelineEventThisVersion(1);
-
-    await eppPage.navigateToVersion(1, true);
+    await eppPage.gotoArticlePage({ version: 1 });
+    await eppPage.reloadAndAssertStatus(200);
     await eppPage.assertDoi('https://doi.org/10.7554/000001.1');
 
     await eppPage.gotoArticlePage({ status: 200 });
     await eppPage.assertTitleText('OpenApePose: a database of annotated ape photographs for pose estimation (revised)');
     await eppPage.assertArticleStatusText('Revised by authors');
     await eppPage.assertDoi('https://doi.org/10.7554/000001.2');
-
-    await eppPage.expandTimeline();
-
-    await eppPage.assertTimelineDetailText(1, 'v2');
-    await eppPage.assertTimelineEventThisVersion(1);
 
     await eppPage.navigateToReviewsTab();
     await eppPage.assertPeerReviewProcess('revised');
