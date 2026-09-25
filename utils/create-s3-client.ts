a85a0ecefb5880098c501cd57b7c7099e962881a
LinaKind <l.kinduryte@elifesciences.org>
2026-09-25 12:41:10 +0300

    Rename to match implementation
    https://github.com/eLifePathways/issues/issues/15

--- utils/create-s3-client.ts
+++ utils/create-s3-client.ts
@@ -2,8 +2,8 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { config } from './config';
 
 export const createS3Client = (): S3Client => new S3Client({
-  credentials: { accessKeyId: 'minio', secretAccessKey: 'miniotest' },
-  endpoint: config.minio_url,
+  credentials: { accessKeyId: 's3mock', secretAccessKey: 's3mock' },
+  endpoint: config.s3mock_url,
   forcePathStyle: true,
   region: 'us-east-1',
 });
